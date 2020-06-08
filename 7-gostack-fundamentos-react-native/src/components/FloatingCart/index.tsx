6daa2d6e1ad397acd8b20c26c9c75370c6dc255a
Allan <allan.g.galdino@outlook.com>
2020-06-08 18:45:01 -0300

    Challenge 7 complete

---
+++ 7-gostack-fundamentos-react-native/src/components/FloatingCart/index.tsx
@@ -0,0 +1,65 @@
+import React, { useMemo } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  CartPricing,
+  CartButton,
+  CartButtonText,
+  CartTotalPrice,
+} from './styles';
+
+import formatValue from '../../utils/formatValue';
+
+import { useCart } from '../../hooks/cart';
+
+// Calculo do total
+// Navegação no clique do TouchableHighlight
+
+const FloatingCart: React.FC = () => {
+  const { products } = useCart();
+
+  const navigation = useNavigation();
+
+  const cartTotal = useMemo(() => {
+    // RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    // RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce((accumulator, product) => {
+      const productQuantity = product.quantity;
+
+      return accumulator + productQuantity;
+    }, 0);
+
+    return totalItems;
+  }, [products]);
+
+  return (
+    <Container>
+      <CartButton
+        testID="navigate-to-cart-button"
+        onPress={() => navigation.navigate('Cart')}
+      >
+        <FeatherIcon name="shopping-cart" size={24} color="#fff" />
+        <CartButtonText>{`${totalItensInCart} itens`}</CartButtonText>
+      </CartButton>
+
+      <CartPricing>
+        <CartTotalPrice>{cartTotal}</CartTotalPrice>
+      </CartPricing>
+    </Container>
+  );
+};
+
+export default FloatingCart;
